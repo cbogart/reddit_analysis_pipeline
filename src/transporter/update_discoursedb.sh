593338dbdb4219a0278756c75c5e194104e1c975
Christopher Bogart <cbogart@andrew.cmu.edu>
2020-07-31 13:47:22 -0400

    Unfinished new work as of Nov 2019

--- src/transporter/update_discoursedb.sh
+++ src/transporter/update_discoursedb.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 mkdir -p /tmp
-cd /usr/src/pipeline
-POSTSFILE=/tmp/posts`date +%Y%m%d_%H%M`.csv
-echo "---------STARTING----------" `date` 
-python3.7 src/transporter/gen_ddb_csv.py -csv $POSTSFILE -l 500 -m reddit-mattress -d mattress -S /usr/src/pipeline/resources/subreddit_groups/forums_mattress.txt 
-echo "--------IMPORTING----------" `date` 
-cd /usr/src/pipeline/discoursedb/discoursedb-core/discoursedb-io-csv
-java -cp target/oneThreadedForumCsv-0.9-SNAPSHOT.jar:target/classes:target/dependency/* edu.cmu.cs.lti.discoursedb.io.csvimporter.CsvImportApplication $POSTSFILE posts --jdbc.database=discoursedb_ext_mattress  
-echo "--------DONE----------" `date` 
+cd /pipeline
+POSTSFILE=tmp/posts`date +%Y%m%d_%H%M`.csv
+for forum in resources/subreddit_groups/forums_*.txt
+do
+    ddbname=`echo $forum | sed -e 's/.*\/forums_\(.*\)\.txt/\1/'`
+    echo "---------STARTING----------" $ddbname $forum `date` >> logs/ddb_log.txt
+    python3.7 src/transporter/gen_discoursedb_csv.py -o $POSTSFILE -d $ddbname -S $forum &>> logs/ddb_log.txt
+    echo "--------IMPORTING----------" `date` >> logs/ddb_log.txt
+    bash /pipeline/discoursedb/discoursedb-io-csv/import_posts $POSTSFILE discoursedb_ext_$ddbname &>> logs/ddb_log.txt
+    #java -cp target/oneThreadedForumCsv-0.9-SNAPSHOT.jar:target/classes:target/dependency/* edu.cmu.cs.lti.discoursedb.io.csvimporter.CsvImportApplication $POSTSFILE posts --jdbc.database=discoursedb_ext_$ddbname
+    echo "--------DONE----------" `date` >> logs/ddb_log.txt
+done
